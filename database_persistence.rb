bf95c5414be5b555245a7c19ff90d1eccec603e6
Melissa Kaspshak <mj2tiger@gmail.com>
2018-06-24 18:42:58 -0500

    Updated Gemfile.

--- database_persistence.rb
+++ database_persistence.rb
@@ -4,10 +4,10 @@ class DatabasePersistence
 
   def initialize(logger)
     @db = if Sinatra::Base.production?
-      PG.connect(ENV['DATABASE_URL'])
-    else
-      PG.connect(dbname: "todos")
-    end
+            PG.connect(ENV['DATABASE_URL'])
+          else
+            PG.connect(dbname: "todos")
+          end
     @logger = logger
   end
   
